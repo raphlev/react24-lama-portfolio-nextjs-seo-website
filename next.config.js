ba981dbc124ef0cd841b07d986dd4d901e3d70a9
raphlev <raphleveque@hotmail.fr>
2021-11-06 15:28:42 +0100

    u

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost", "images.pexels.com"],
+    domains: ["localhost", "images.pexels.com","app.netlify.com"],
     loader: "imgix",
     path: "",
   },
